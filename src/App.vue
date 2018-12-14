e9861e43d9a98875e71f8ed59472b1c1880536f2
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-12-14 13:26:56 +0000

    wip: scaffold app for login & regiser with new landing page.

--- src/App.vue
+++ src/App.vue
@@ -28,14 +28,14 @@ export default {
 }
 
 
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 60px;
   font-size: 24px;
-}
+} */
 
 .logo {
   display: block;
---
+++ src/components/marketing/LandingPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Landing page content here.
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
